a86a5e095bad3b7246955412259d2d8d582f6177
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-06-02 14:25:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use((err, req, res, next) => {
 });
 
 // ================= START SERVER =================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5026;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
